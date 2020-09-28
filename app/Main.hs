902ec85c85d606e168f015fbdf187aee85da9e0e
Felix Linker <linkerfelix@gmail.com>
2020-09-28 13:24:53 +0200

    Update main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,22 @@
 module Main where
 
+import Data.Char (isSpace)
 import Lib
+import Numeric
+import System.Environment
 import qualified System.Random as R
 
 genI :: Integer -> IO Integer
 genI upperBound = R.randomRIO (0 :: Integer, upperBound)
 
 main :: IO ()
-main = return ()
+main = do
+    gS : _ <- getArgs
+    let g = fst $ head $ readHex gS
+    pS <- getContents
+    let p = fst $ head $ readHex $ filter (not . isSpace) pS
+    a <- genI p
+    b <- genI p
+    let (pkA, pkB, sk) = dh a b g p
+    putStrLn $ showHex sk ""
+    return ()
